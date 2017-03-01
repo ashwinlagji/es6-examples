a6946a01ca6cb71434669057d22737550886c005
ashwinlagji <ashwin_lagji@persistent.co.in>
2017-03-01 12:36:43 +0530

    Hello-world and initial typescript setup

---
+++ scripts/0_test-hello-world.js
@@ -0,0 +1,20 @@
+/*
+    Hello world example for testing the typescript compiler
+    
+    Install the typescript compiler using npm
+        : npm install -g typescript
+
+    TypeScript can be compiled to es5 by using command
+        : tsc <file_name>.ts
+    
+    The typescript project config can be found in tsconfig.json
+    
+    For compiling all files in project folder use command
+        : tsc
+    from root folder
+    
+    check the outDir config in tsconfig.json for output files destination
+
+*/
+console.log("hello world");
+//# sourceMappingURL=0_test-hello-world.js.map
---
+++ scripts/1_var-and-let.js
@@ -0,0 +1,5 @@
+/*
+
+
+*/
+//# sourceMappingURL=1_var-and-let.js.map
